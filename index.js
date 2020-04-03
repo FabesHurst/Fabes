758a9700e2d27e8008ee0b6cbac2a6b64907b34c
FabesHurst <63106514+FabesHurst@users.noreply.github.com>
2020-04-03 14:07:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+let name;
+name = "Fabian"
+let age = 15;
+let school = "code nation";
+
+
+
+
+console.log("Hi, my name is" + name);
+
+
+
+console.log("i am" + age + "years old");
+console.log("i attend" + school);
+
+let numberOne; 
+let numberTwo;
+let total;
+
+const addition = (numberOne, numberTwo) => {
+
+total = numberOne + numberTwo;
+
+console.log(total);
+};
+
+addition(20, 30);
+addition(30,70);
+addition(40, 15);
+addition(400, 150)
